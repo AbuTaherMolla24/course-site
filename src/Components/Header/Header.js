7b1838187245e601e9b4d9931535493a9e191bf2
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-08-28 01:13:57 +0600

    end

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+
+        
+            <h1 className="text-center">Welcome to our edu site website</h1>
+            <h4 className="text-center">প্রফেশনাল ওয়েব ডেভেলপার হওয়ার জন্য যা যা লাগবে তার সবকিছুই সহজ করে গিলিয়ে খাওয়ানো হবে এই কোর্সে।</h4>
+            
+        </div>
+    );
+};
+
+export default Header;
